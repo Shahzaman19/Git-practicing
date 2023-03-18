65b78fe7517e83ceb2bf0d149f5d063e30d87998
[Shahzaman] <shahzaman.aftab@gmail.com>
2023-03-18 13:40:02 +0500

    commiting both files

--- 1.js
+++ 1.js
@@ -1,28 +1,6 @@
 const number = [8,3,10,1,5];
 
-// let x = number.map((Element) =>{
-    //     return Element * Element
-    // })
-    // console.log(x);
-    
-    // let x = number.filter((Element) => {
-        //     return Element > 5;
-        // })
-        // console.log(y);
-        
-        // let x = number.reduce((a,b) => a + b)
-        
-        // let x = number.forEach((i,Element)=>{
-            //     console.log(i,Element);
-            // })
-            
-            //  let x = number.sort((a,b) => b - a); 
-            
-// let x = number.find(element =>  element > 4);
-// let x = number.some(Element => Element > 8);
-
-// let x = number.every(Element => Element > 0)
-// let x = number.indexOf(3);
+console.log("zazaz");
 
 let x = number.join('"-"')
 
